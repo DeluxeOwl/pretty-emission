73e9d9f2122a171402ba26dc3b99234b72ae90bf
DeluxeOwl <nospamplease0@protonmail.com>
2023-07-24 17:45:12 +0300

    feat: added jotai, cva

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { twMerge } from "tailwind-merge";
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -10,16 +11,77 @@ import useFontsLoaded from "./hooks/useFontsLoaded";
 import init from "./init";
 import { cva } from "class-variance-authority";
 
-const buttonStyles = cva(["px-3", "py-1.5", "bg-sky-300", "rounded-lg"]);
+import { atom, useAtom } from "jotai";
+import { useCallback } from "react";
+
+const PrimaryColors = ["bg-sky-500", "bg-amber-600"] as const;
+type PrimaryColor = (typeof PrimaryColors)[number];
+
+const PrimaryPressed = ["bg-sky-500/80", "bg-amber-500/80"] as const;
+type PrimaryColorPressed = (typeof PrimaryPressed)[number];
+
+// should add a default as well
+// maybe name it color scheme
+type Theme = {
+  primaryColor: PrimaryColor;
+  primaryColorPressed: PrimaryColorPressed;
+};
+type Themes = Record<string, Theme>;
+
+const t: Themes = {
+  sky: {
+    primaryColor: "bg-sky-500",
+    primaryColorPressed: "bg-sky-500/80",
+  },
+  amber: {
+    primaryColor: "bg-amber-600",
+    primaryColorPressed: "bg-amber-500/80",
+  },
+};
+
+const themeAtom = atom<Theme>(t.sky);
 
 init();
 
 export function Button({ children }: { children: string }) {
+  const [theme, setTheme] = useAtom(themeAtom);
+
+  // not good, should also get its type
+  const buttonStyles = useCallback(
+    cva(["px-3", "py-1.5", "rounded-lg", "border"], {
+      variants: {
+        intent: {
+          primary: ["border-sky-200", theme.primaryColor],
+        },
+        intentAction: {
+          primary: [theme.primaryColorPressed],
+        },
+      },
+    }),
+    [theme]
+  );
+
   return (
     <Pressable
-      style={tw.style(buttonStyles())}
-      onPress={() => console.log("pressed")}>
-      <Text style={{ fontFamily: "Satoshi-Medium" }}>{children}</Text>
+      style={({ pressed }) => {
+        return tw.style(
+          twMerge(
+            buttonStyles({
+              intent: "primary",
+              intentAction: pressed ? "primary" : null,
+            })
+          )
+        );
+      }}>
+      {({ pressed }) =>
+        pressed ? (
+          <Text style={{ fontFamily: "SatoshiMedium" }}>
+            {children} but PRESSED
+          </Text>
+        ) : (
+          <Text style={{ fontFamily: "SatoshiMedium" }}>{children}</Text>
+        )
+      }
     </Pressable>
   );
 }
@@ -34,7 +96,7 @@ export function Home() {
       })}
       onLayout={onLayoutRootView}>
       <Text style={styles.withFont}>Hello!</Text>
-      <Button>Press me please</Button>
+      <Button>I am just a simple button ...</Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -56,6 +118,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   withFont: {
-    fontFamily: "Satoshi-Black",
+    fontSize: 48,
+    fontFamily: "SatoshiBold",
   },
 });
--- hooks/useFontsLoaded.ts
+++ hooks/useFontsLoaded.ts
@@ -7,16 +7,16 @@ export default function useFontsLoaded(): [
   onLayoutRootView: () => Promise<void>
 ] {
   const [fontsLoaded] = useFonts({
-    "Satoshi-Black": require("../assets/fonts/Satoshi-Black.otf"),
-    "Satoshi-BlackItalic": require("../assets/fonts/Satoshi-BlackItalic.otf"),
-    "Satoshi-Bold": require("../assets/fonts/Satoshi-Bold.otf"),
-    "Satoshi-BoldItalic": require("../assets/fonts/Satoshi-BoldItalic.otf"),
-    "Satoshi-Italic": require("../assets/fonts/Satoshi-Italic.otf"),
-    "Satoshi-Light": require("../assets/fonts/Satoshi-Light.otf"),
-    "Satoshi-LightItalic": require("../assets/fonts/Satoshi-LightItalic.otf"),
-    "Satoshi-Medium": require("../assets/fonts/Satoshi-Medium.otf"),
-    "Satoshi-MediumItalic": require("../assets/fonts/Satoshi-MediumItalic.otf"),
-    "Satoshi-Regular": require("../assets/fonts/Satoshi-Regular.otf"),
+    SatoshiBlack: require("../assets/fonts/Satoshi-Black.otf"),
+    SatoshiBlackItalic: require("../assets/fonts/Satoshi-BlackItalic.otf"),
+    SatoshiBold: require("../assets/fonts/Satoshi-Bold.otf"),
+    SatoshiBoldItalic: require("../assets/fonts/Satoshi-BoldItalic.otf"),
+    SatoshiItalic: require("../assets/fonts/Satoshi-Italic.otf"),
+    SatoshiLight: require("../assets/fonts/Satoshi-Light.otf"),
+    SatoshiLightItalic: require("../assets/fonts/Satoshi-LightItalic.otf"),
+    SatoshiMedium: require("../assets/fonts/Satoshi-Medium.otf"),
+    SatoshiMediumItalic: require("../assets/fonts/Satoshi-MediumItalic.otf"),
+    SatoshiRegular: require("../assets/fonts/Satoshi-Regular.otf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
