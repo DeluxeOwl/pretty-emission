bd142e6f224506299a7cf557aafc6e4e7e790168
DeluxeOwl <nospamplease0@protonmail.com>
2023-07-28 01:02:44 +0300

    feat: any design system ffs

--- hooks/useStyles.ts
+++ hooks/useStyles.ts
@@ -1,15 +1,7 @@
 import { type ThemeProps } from "../styles/design-system";
 import { useTheme } from "./useTheme";
 
-import type { cva } from "class-variance-authority";
 import { useCallback } from "react";
-
-type CvaType<T> = typeof cva<T>;
-type CvaParams<T> = Parameters<CvaType<T>>;
-
-type ClassValue = CvaParams<any>[0];
-type Cfg<T> = CvaParams<T>[1];
-type Ret<T> = ReturnType<CvaType<T>>;
 
 export function useStyles(callback: (t: ThemeProps) => any) {
   const theme = useTheme();
