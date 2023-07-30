15e6eafebeb37a69b352ce14ef31bcf8479d90a7
DeluxeOwl <nospamplease0@protonmail.com>
2023-07-30 17:51:35 +0300

    feat: figured some button opacities

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,30 +1,39 @@
 import tw from "lib/tailwind";
 
-import { buttonStyles } from "hooks/useButtonStyles";
+import { buttonStyles, outerViewStyles } from "hooks/useButtonStyles";
 import { Pressable, Text, View } from "react-native";
 import { useTheme } from "hooks/useTheme";
 
 export function Button({ children }: { children: string }) {
   const pressableStyle = ({ pressed }: { pressed: boolean }) =>
     buttonStyles({
-      intent: "primary",
-      intentAction: pressed ? "primary" : null,
+      variant: "secondary",
+      isPressed: pressed,
     });
 
   const t = useTheme();
 
   return (
-    <View style={tw`overflow-hidden rounded-2xl shadow-2xl`}>
-      {/* If you want to customize the ripple, useTheme or depending on the button props*/}
-      <Pressable style={pressableStyle} android_ripple={{ borderless: true }}>
-        {({ pressed }) => (
-          <Text
-            style={tw`self-center ${
+    <View
+      style={outerViewStyles({
+        variant: "secondary",
+      })}>
+      <Pressable style={pressableStyle}>
+        {({ pressed }) => {
+          console.log(
+            tw`self-center ${
               pressed ? t.defaultTextStrongPressed : t.defaultTextStrong
-            }`}>
-            {children}
-          </Text>
-        )}
+            }`
+          );
+          return (
+            <Text
+              style={tw`self-center text-xl ${
+                pressed ? t.defaultTextStrongPressed : t.defaultTextStrong
+              }`}>
+              {children}
+            </Text>
+          );
+        }}
       </Pressable>
     </View>
   );
--- hooks/useButtonStyles.ts
+++ hooks/useButtonStyles.ts
@@ -3,35 +3,52 @@ import { atom } from "jotai";
 import { styles } from "styles/lib";
 import type { VarProps } from "util/types.ts";
 import { themeAtom } from "./useTheme";
-import tw from "lib/tailwind";
+
+const outerViewStylesAtom = atom((get) => {
+  const t = get(themeAtom);
+  return cva(["overflow-hidden"], {
+    variants: {
+      variant: {
+        primary: ["rounded-2xl", "shadow-2xl"],
+        secondary: [],
+      },
+    },
+  });
+});
+
+export const outerViewStyles = styles(outerViewStylesAtom);
+export type OuterViewProps = VarProps<typeof outerViewStylesAtom>;
 
 const buttonStylesAtom = atom((get) => {
   const t = get(themeAtom);
 
-  return cva(
-    [
-      "px-3",
-      "py-2",
-      "rounded-2xl",
-      "border-[0.5px]",
-      "w-full",
-      "h-12",
-      "justify-center",
-    ],
-    {
-      variants: {
-        intent: {
-          primary: [t.defaultBackgroundWeak, t.defaultBorderWeak],
-        },
-        intentAction: {
-          primary: [
-            tw.prefixMatch("ios") &&
-              t.defaultBackgroundWeakPressed + " " + t.defaultTextStrongPressed,
-          ],
-        },
+  return cva(["px-3", "py-2", "h-12", "justify-center", "self-stretch"], {
+    variants: {
+      variant: {
+        primary: [
+          t.defaultBackgroundWeak,
+          t.defaultBorderWeak,
+          "border-[0.5px]",
+        ],
+        secondary: [],
       },
-    }
-  );
+      isPressed: {
+        true: {},
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "primary",
+        isPressed: true,
+        class: [t.defaultBackgroundWeakPressed, t.defaultTextStrongPressed],
+      },
+      {
+        variant: "secondary",
+        isPressed: true,
+        class: ["opacity-70"],
+      },
+    ],
+  });
 });
 
 export const buttonStyles = styles(buttonStylesAtom);
